48074a268816ddf968bd4f5279d71d178a89b844
Felipe Santana <l.felipesantana.ff@gmail.com>
2025-12-08 14:58:30 -0300

    inicio do projeto

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'
+import { FormsModule } from '@angular/forms';
+import { Cliente } from './cliente'
+import { ClienteService } from '../cliente.service';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [ 
+     FlexLayoutModule,
+     MatCardModule, 
+     FormsModule, 
+     MatFormFieldModule,
+     MatInputModule,
+     MatIconModule, 
+     MatButtonModule],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.scss'
+})
+export class CadastroComponent {
+
+  cliente: Cliente = Cliente.newCliente();
+
+  constructor(private service: ClienteService){
+
+  }
+
+  salvar(){
+    this.service.salvar(this.cliente);
+  }
+
+
+
+}
---
+++ src/app/consulta/consulta.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { MatInputModule} from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatIconModule } from '@angular/material/icon'
+import { FormsModule } from '@angular/forms'
+import { MatTableModule } from '@angular/material/table'
+
+@Component({
+  selector: 'app-consulta',
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatTableModule,
+    FlexLayoutModule,
+    FormsModule
+
+  ],
+  templateUrl: './consulta.component.html',
+  styleUrl: './consulta.component.scss'
+})
+export class ConsultaComponent {
+
+}
