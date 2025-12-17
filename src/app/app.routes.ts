97d69af262cb5acda4e4cc877a724cd1569093ec
Felipe Santana <l.felipesantana.ff@gmail.com>
2025-12-16 21:25:45 -0300

    atualizando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { ConsultaComponent } from './consulta/consulta.component';
 
 export const routes: Routes = [
-    { path: 'cadastro', component: CadastroComponent},
-    { path: 'consulta', component: ConsultaComponent }
+     { path: '', redirectTo: 'cadastro', pathMatch: 'full' },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'consulta', component: ConsultaComponent },
 ];
